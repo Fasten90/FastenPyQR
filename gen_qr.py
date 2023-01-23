b7a86e637eea5164937aacf242740041b99ec6bf
Fasten90 <fasten90@gmail.com>
2023-01-23 23:25:08 +0100

    Generalize QR code generation

--- gen_qr.py
+++ gen_qr.py
@@ -1,15 +1,21 @@
 import qrcode
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=10,
-    border=4,
-)
-qr.add_data('Some data')
-qr.make(fit=True)
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("some_file.png")
+def generate_qr(info_text, save_path):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(info_text)
+    qr.make(fit=True)
 
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(save_path)
+
+
+if __name__ == '__main__':
+    print('Generate QR')
+    generate_qr('TestData', 'test/test_file.png')
 
