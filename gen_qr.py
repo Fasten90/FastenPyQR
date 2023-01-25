b65f3747a1ad16ce3f28818e459a0fe2b21c0cb1
Fasten90 <fasten90@gmail.com>
2023-01-25 23:43:08 +0100

    Update image merging

--- gen_qr.py
+++ gen_qr.py
@@ -5,20 +5,23 @@ from PIL import Image, ImageFont, ImageDraw
 
 def merge_images(qr_img, logo_img):
     """ https://stackoverflow.com/questions/63355160/how-to-concatenate-two-images-one-on-top-of-the-other-in-python """
+    """ Merge qr + logo (left + right) """
 
     # Scale the logo image. The width of the scaled logo shall be equal to the width of the QR code
     qr_width, qr_height = qr_img.size
     logo_current_width, logo_current_height = logo_img.size
-    scaling_factor = qr_width / logo_current_width
-    logo_new_height = int(scaling_factor * logo_current_height)
-    logo_new_size = (qr_width, logo_new_height)
+    #scaling_factor = qr_width / logo_current_width
+    #logo_new_height = int(scaling_factor * logo_current_height)
+    logo_new_width = int(qr_width + logo_current_width)
+    logo_new_height = max(logo_current_height, qr_height)
+    logo_new_size = (logo_new_width, logo_new_height)
     logo_img = logo_img.resize(logo_new_size)
 
     # Create an empty image to paste both images on
     margin = 20
-    result = Image.new(mode='RGB', size=(qr_width, qr_height + margin + logo_new_height), color='white')
+    result = Image.new(mode='RGB', size=(logo_new_width, logo_new_height), color='white')
     result.paste(logo_img, (0, 0))
-    result.paste(qr_img, (0, qr_height + margin))
+    result.paste(qr_img, (logo_current_width, 0))
     return result
 
 
@@ -62,11 +65,12 @@ def main():
     print('Generate QR')
     save_path = 'test/test_file.png'
     # Generate QR code image
-    qr_img = generate_qr('TestData')
+    qr_text = 'TestData'
+    qr_img = generate_qr(qr_text)
     # Load the logo
     logo_img = load_image('template.png')
     # Edit image
-    img_text = [ "test_text", "line 2..." ]
+    img_text = [ 'test_text', 'line 2...' ]
     edited_image = add_text_to_image(logo_img, img_text)
     # Merge
     merged_img = merge_images(qr_img, edited_image)
